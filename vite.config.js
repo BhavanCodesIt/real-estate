c07f756256948c7603d1443997b7bf325a3b319d
BhavanPatil <bhavanpatil786@gmail.com>
2025-04-13 19:18:53 +0530

    fix: update gitignore, main.jsx and vite config for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/real-estate/',
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), viteStaticCopy({
+    targets: [
+      { src: 'public/*', dest: '' }
+    ]
+  }), tailwindcss()],
 })
