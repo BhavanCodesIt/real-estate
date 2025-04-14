9fe425df820aec6a8a0d58b4d5c9d121aadcd9e8
BhavanPatil <bhavanpatil786@gmail.com>
2025-04-14 19:51:53 +0530

    Move header_img.png to public folder and update references

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/real-estate/',
-  plugins: [react(), viteStaticCopy({
-    targets: [
-      { src: 'src/assets/*', dest: 'assets/' }
-    ]
-  }), tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+});
