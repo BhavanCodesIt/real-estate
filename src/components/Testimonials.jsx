b2f493bea6547e86f4c3c5b1a6719bd671042ace
BhavanPatil <bhavanpatil786@gmail.com>
2025-04-14 10:24:04 +0530

    Fixed asset exports, corrected Navbar links, renamed Testimonails to Testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import { assets, testimonialsData } from '../assets/assets'
 import { motion } from "motion/react"
 
-const Testimonals = () => {
+const Testimonials = () => {
   return (
     <motion.div 
     initial={{opacity: 15, x:100}}
         transition={{duration: 1}}
         whileInView={{opacity: 1, x:0}}
         viewport={{once: true}}
-    className='container mx-auto py-10 lg:px-32 w-full overflow-hidden' id="Testimonails" > 
-      <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Customer <span className='underline underline-offset-4 decoration-1 under font-light'>Testimonals</span></h1>
+    className='container mx-auto py-10 lg:px-32 w-full overflow-hidden' id="Testimonials" > 
+      <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Customer <span className='underline underline-offset-4 decoration-1 under font-light'>Testimonials</span></h1>
       <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Real Stories from those Who Found Home with Us</p>
       
       <div className='flex flex-wrap justify-center gap-8'>
@@ -32,4 +32,4 @@ const Testimonals = () => {
   )
 }
 
-export default Testimonals
+export default Testimonials
